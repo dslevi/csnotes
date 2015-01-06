45b8e62b0c70c1800110a58d283e25f5d7def43d
Danielle Levi <dslevi12@gmail.com>
2015-01-05 20:35:00 -0800

    adding ejs

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
+//views engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.render('layout')
 });
 
 app.listen(app.get('port'), function() {
